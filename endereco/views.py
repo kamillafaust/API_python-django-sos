352fe058dc9a3e0741ec8eb7e4b5a6f2fa986554
Kamilla Faust <kamilla.cfaust@gmail.com>
2022-03-07 21:51:30 -0300

    update

--- endereco/views.py
+++ endereco/views.py
@@ -3,6 +3,7 @@ from endereco.models import Endereco
 from endereco.serializers import EnderecoSerializer
 
 class EnderecoViewSet(viewsets.ModelViewSet)
-    serializer = EnderecoSerializer
+    serializer_class = EnderecoSerializer
     queryset = Endereco.objects.all()
     http_method_names = ['get', 'post', 'path']
+
